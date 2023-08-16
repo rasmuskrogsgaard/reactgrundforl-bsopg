ba58e93c43a6abdd59b107492a7dbf53c08a444f
Rasmus Krogsgaard <113165532+rasmuskrogsgaard@users.noreply.github.com>
2023-08-17 01:04:52 +0200

    edringer

--- grundforloebproeve/src/App.jsx
+++ grundforloebproeve/src/App.jsx
@@ -1,15 +1,17 @@
 import HeroMain from './components/Hero/Hero.jsx'
 import './App.css'
 import NavHeader from './components/Nav/Nav.jsx'
+import TContent from './components/TopContent/TContent.jsx'
 const App = () => {
   return (
 <div className='App'>
   
   <HeroMain/>
   <NavHeader/>
+  <TopContent/>
   
+
 </div>
   )
-
 }
 export default App
---
+++ grundforloebproeve/src/components/TopContent/TopContent.jsx
@@ -0,0 +1,14 @@
+import style from './TopContent/tc.module.scss'
+
+const TopContent = () => {
+    return(
+<div className={style.tcontent}>
+Top 
+
+
+</div>
+
+
+    )
+}
+export default TopContent
