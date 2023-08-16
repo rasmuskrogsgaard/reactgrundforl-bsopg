43f713db9ab199ff4d19593c006721c828492a65
Rasmus Krogsgaard <113165532+rasmuskrogsgaard@users.noreply.github.com>
2023-08-16 14:40:10 +0200

    styling

--- grundforloebproeve/src/components/Nav/Nav.jsx
+++ grundforloebproeve/src/components/Nav/Nav.jsx
@@ -1,7 +1,7 @@
-
+import style from './nav.module.scss';
 const NavHeader = () => {
 return(
-<nav>
+<nav className={style.main_navbar}>
     <li><a href="#home">Verdensmålene</a></li>
     <li><a href="#home">Delmålene</a></li>
     <li><a href="#home">Udfordringer</a></li>
---
+++ grundforloebproeve/src/components/TopContent/Tcontent.jsx
@@ -0,0 +1,35 @@
+
+
+const TopContent = () => {
+    return(
+<article className="tcontent">
+<p><h1>DE 17 VERDENSMÅL</h1>
+
+På FN topmødet i New York i 2015 vedtog verdens stats- og regeringsledere en hidtil uset ambitiøs og samfundsforandrende udviklingsdagsorden, også kaldt 2030 dagsordenen.
+
+Frem til 2030 skal denne dagsorden sætte kurs mod en mere bæredygtig udvikling for både mennesker og planeten, vi bor på.</p>
+
+<div className={tc_grid}>
+<p>Verdensmålene forpligter.
+
+Medlemslandende forpligter sig til helt at afskaffe fattigdom og sult i verden, sikre god uddannelse og sundhed til alle, reducere ulighed, fremme ligestilling, anstændige jobs, bæredygtig vækst og forbrug.
+
+Den nye dagsorden anerkender således at social, økonomisk og miljømæssig udvikling, fred, sikkerhed og internationalt samarbejde er tæt forbundet, og at det kræver en integreret indsats at opnå holdbare udviklingsresultater.</p>
+
+<p>
+Verdensmålene gælder alle.
+
+Målene gælder alle lande - både rige og fattige - dvs. de er universelle.
+
+Udfordringer som social, økonomisk  marginalisering, stigende ulighed, fødevareusikkerhed,  ulig adgang til grundlæggende naturressourcer,og har regionale og globale konsekvenser.
+
+Det er derfor afgørende at alle lande leverer og løfter opgaven i fællesskab.</p></div>
+
+
+
+
+
+</article>
+)
+}
+export default TopContent
